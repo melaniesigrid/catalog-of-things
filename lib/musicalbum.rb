24fefe29da34baf96e481d327e3a9c6cc6498e18
Fabricio Garcia <fabgrel10@gmail.com>
2022-04-26 11:26:15 -0500

    Create MusicAlbum class and its methods

---
+++ lib/musicalbum.rb
@@ -0,0 +1,14 @@
+require_relative 'item'
+
+class MusicAlbum < Item
+  attr_accessor :on_spotify
+
+  def initialize(on_spotify, publish_date, archived, id: Random.rand(1..1000))
+    super(publish_date, archived, id: id)
+    @on_spotify = on_spotify
+  end
+
+  def can_be_archived?
+    super && @on_spotify
+  end
+end
