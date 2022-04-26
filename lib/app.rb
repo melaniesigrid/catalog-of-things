025dfe529c3d1c0d8228637fedff84da98f382e4
gabyse1 <gaby.es.iaa@gmail.com>
2022-04-26 16:07:49 -0500

    Create the add_gam and list_games method in app.rb class file

--- lib/app.rb
+++ lib/app.rb
@@ -1,4 +1,18 @@
+require_relative '../helpers/validations'
+require_relative './game/author'
+require_relative './game/game'
+require_relative './game/game_manager'
+require 'json'
+
 class App
+  include Validations
+  include GameAuthorManager
+
+  def initialize
+    @authors = []
+    @games = []
+  end
+
   def select_option
     choice = gets.chomp.strip.to_i
     case choice
@@ -38,6 +52,30 @@ class App
       add_music_album
     when 9
       add_game
+    end
+  end
+
+  def add_game
+    print "\nPublish date (yyyy/mm/dd): "
+    publish_date = validates_date
+    print 'Multiplayer (y/n): '
+    multiplayer = gets.chomp.to_s
+    multiplayer = multiplayer.downcase == 'y'
+    print 'Last played at (yyyy/mm/dd): '
+    last_played_at = validates_date
+    game = Game.new(multiplayer, last_played_at, publish_date)
+    game.author = select_author
+    @games << game
+    puts "Game created successfully\n\n"
+  end
+
+  def list_games
+    puts 'No game registered yet' if @games.empty?
+    puts "\n----- GAMES -----" unless @games.empty?
+    @games.each_with_index.each do |e, i|
+      author = "#{e.author.first_name} #{e.author.last_name}"
+      prop_game = "Multiplayer: #{e.multiplayer}, Last played at: #{e.last_played_at}"
+      puts "#{i}) Publish date: #{e.publish_date}, Author: #{author}, #{prop_game}"
     end
   end
 end
