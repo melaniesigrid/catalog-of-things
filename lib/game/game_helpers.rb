c204351da264ecf1297c42d8567f848f47efcb68
gabyse1 <gaby.es.iaa@gmail.com>
2022-04-27 12:52:10 -0500

    Fix linter errors

--- lib/game/game_helpers.rb
+++ lib/game/game_helpers.rb
@@ -1,39 +1,34 @@
 module GameHelpers
   def store_data_to_json
-    puts 'store data to json'
     File.write('data/authors.json', JSON.generate(@list_authors))
     File.write('data/games.json', JSON.generate(@list_games))
   end
 
   def load_authors_from_json
-    if File.exist?('data/authors.json')
-      authors_arr = JSON.parse(File.read('data/authors.json'))
-      if authors_arr
-        authors_arr.each do |obj|
-          @list_authors << Author.new(obj['first_name'], obj['last_name'], id: obj['id'])
-        end
-      end
+    return unless File.exist?('data/authors.json')
+
+    authors_arr = JSON.parse(File.read('data/authors.json'))
+    authors_arr.each do |obj|
+      @list_authors << Author.new(obj['first_name'], obj['last_name'], id: obj['id'])
     end
   end
 
   def load_games_from_json
-    if File.exist?('data/games.json')
-      items_arr = JSON.parse(File.read('data/games.json'))
-      if items_arr
-        items_arr.each do |obj|
-          game = Game.new(
-            obj['multiplayer'],
-            obj['last_played_at'],
-            obj['publish_date'],
-            id: obj['id'],
-            archived: obj['archived']
-          )
-          author = @list_authors.find { |e| e.id == obj['author'] }
-          author.add_item(game)
-          @list_games << game
-        end
-      end
+    return unless File.exist?('data/games.json')
+
+    games_arr = JSON.parse(File.read('data/games.json'))
+    games_arr.each do |obj|
+      game = Game.new(
+        obj['multiplayer'],
+        obj['last_played_at'],
+        obj['publish_date'],
+        id: obj['id'],
+        archived: obj['archived']
+      )
+      @list_authors.find { |e| e.id == obj['author'] }.add_item(game)
+      @list_games << game
     end
+    @list_games.each { |e| puts e.archived }
   end
 
   def validates_date
